b00062179d27d14a9cf079eea793d082bcef4ef0
windedge <windedge99@gmail.com>
2023-01-29 07:51:46 +0000

    add scrollbar to OutputScreen

--- src/main/kotlin/scooper/ui/Output.kt
+++ src/main/kotlin/scooper/ui/Output.kt
@@ -1,25 +1,29 @@
 package scooper.ui
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.twotone.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import scooper.util.cursorHand
 import scooper.util.cursorLink
 import scooper.viewmodels.AppsViewModel
 
@@ -28,6 +32,8 @@ fun OutputScreen(onBack: () -> Unit = {}) {
     val appsViewModel: AppsViewModel = KoinJavaComponent.get(AppsViewModel::class.java)
     val state by appsViewModel.container.stateFlow.collectAsState()
     val output = state.output
+    val scrollState = rememberScrollState()
+    val scope = rememberCoroutineScope()
 
     Column {
         Surface(
@@ -35,9 +41,10 @@ fun OutputScreen(onBack: () -> Unit = {}) {
             elevation = 2.dp,
             shape = MaterialTheme.shapes.large
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize().padding(start = 10.dp),
-                contentAlignment = Alignment.CenterStart
+            Row(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 IconButton(onClick = { onBack() }) {
                     Icon(
@@ -47,12 +54,34 @@ fun OutputScreen(onBack: () -> Unit = {}) {
                         tint = MaterialTheme.colors.onSecondary
                     )
                 }
+
+                Button(
+                    onClick = { appsViewModel.intent { reduce { state.copy(output = "") } } },
+                    modifier = Modifier.cursorHand()
+                ) {
+                    Text("Clear")
+                }
             }
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Box(modifier = Modifier.padding(horizontal = 2.dp)) {
+        BoxWithConstraints(modifier = Modifier.padding(horizontal = 2.dp)) {
+            val padding = 10.dp
+            var contentHeight by remember { mutableStateOf(0) }
+            val boxHeight = LocalDensity.current.run { (this@BoxWithConstraints.maxHeight + padding).roundToPx() }
+            val showScrollButtons by remember(contentHeight, boxHeight) {
+                derivedStateOf { contentHeight > boxHeight }
+            }
+
+            BasicTextField(
+                output,
+                {},
+                modifier = Modifier.fillMaxSize().padding(padding).verticalScroll(scrollState),
+                readOnly = true,
+                textStyle = MaterialTheme.typography.caption.copy(fontSize = 13.sp, color = Color.DarkGray),
+                onTextLayout = { contentHeight = it.size.height }
+            )
             Canvas(modifier = Modifier.fillMaxSize()) {
                 drawRoundRect(
                     color = Color.LightGray,
@@ -60,13 +89,51 @@ fun OutputScreen(onBack: () -> Unit = {}) {
                     style = Stroke(width = 0.5f)
                 )
             }
-            BasicTextField(
-                output,
-                {},
-                modifier = Modifier.fillMaxSize().padding(10.dp),
-                readOnly = true,
-                textStyle = MaterialTheme.typography.caption.copy(fontSize = 13.sp, color = Color.DarkGray)
+
+            VerticalScrollbar(
+                rememberScrollbarAdapter(scrollState),
+                modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd)
             )
+
+            if (showScrollButtons) {
+                ScrollButtons(
+                    modifier = Modifier.Companion.align(Alignment.BottomEnd).padding(20.dp),
+                    scope,
+                    scrollState
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ScrollButtons(modifier: Modifier = Modifier, scope: CoroutineScope, scrollState: ScrollState) {
+    Column(modifier = modifier) {
+        FloatingActionButton(
+            onClick = {
+                scope.launch {
+                    scrollState.animateScrollTo(0)
+                }
+            },
+            shape = RoundedCornerShape(50),
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = Modifier.size(30.dp).cursorHand()
+        ) {
+            Icon(Icons.Filled.KeyboardArrowUp, "")
+        }
+
+        Spacer(modifier = Modifier.height(5.dp))
+        FloatingActionButton(
+            onClick = {
+                scope.launch {
+                    scrollState.animateScrollTo(scrollState.maxValue)
+                }
+            },
+            shape = RoundedCornerShape(50),
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = Modifier.size(30.dp).cursorHand()
+        ) {
+            Icon(Icons.Filled.KeyboardArrowDown, "")
         }
     }
 }
